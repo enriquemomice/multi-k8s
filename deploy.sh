d656e5fa63fdc8dcdb6ce859a29e1b5362265a99
enrique <enrique@momice.com>
2020-02-04 13:47:05 +0100

    adding env variable to disable prompt gsdk

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
-docker build -t enriquemomice/multi-client -f .client/Dockerfile ./client
-docker build -t enriquemomice/multi-server -f .server/Dockerfile ./server
-docker build -t enriquemomice/multi-worker -f .worker/Dockerfile ./worker
+docker build -t enriquemomice/multi-client -f ./client/Dockerfile ./client
+docker build -t enriquemomice/multi-server -f ./server/Dockerfile ./server
+docker build -t enriquemomice/multi-worker -f ./worker/Dockerfile ./worker
 docker push enriquemomice/multi-client
 docker push enriquemomice/multi-server
 docker push enriquemomice/multi-worker
